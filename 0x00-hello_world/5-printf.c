4de082d423574bd7b591ad32a33db506598465b6
sirrnathan <xoyiyanceba@gmail.com>
2023-02-16 03:33:02 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - A program that prints a sentence followed by a new line using printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
