367d936a39414fc88937bbba2c205b8e590e98c2
sirrnathan <xoyiyanceba@gmail.com>
2023-02-21 03:02:33 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet, in lowercase
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}	
+}
