9835784e42961d57d0064c19ed4a5042ddbb37bb
sirrnathan <xoyiyanceba@gmail.com>
2023-02-21 03:24:02 -0800

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - _islower checks for lowecase letter
+ * @c: character to check the case
+ * Return: Always 0 or 1.
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c >= 122);
+}
