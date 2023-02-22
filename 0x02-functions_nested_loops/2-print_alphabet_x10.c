63e860eb62b48c4fe0c351599e131416bd955fa0
sirrnathan <xoyiyanceba@gmail.com>
2023-02-22 04:29:53 -0800

    0 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  * Return: Always 0.
  */
 
@@ -18,5 +18,5 @@ void print_alphabet_x10(void)
 	}
 	_putchar('\n');
 	i++;
-	}	
+	}
 }
