88888e9c65b9c82bc5b5f76c205f3c8ba38aee76
sirrnathan <xoyiyanceba@gmail.com>
2023-02-27 03:36:47 -0800

    6. Half the lies they tell

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - function should print only one character out of two
+ * puts2 - prints every other character of a string
  * starting with the first one
  * @str: input
  * Return: print
