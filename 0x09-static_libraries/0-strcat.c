9d70575b67131dfff12b624824f2674298f0e6e3
sirrnathan <xoyiyanceba@gmail.com>
2023-03-10 05:41:31 -0800

    .o file

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -9,23 +9,23 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index;
+	int i;
 	int j;
 
-	index = 0;
+	i = 0;
 	j = 0;
 
-	while (dest[index] != '\0')
+	while (dest[i] != '\0')
 	{
-		index++;
+		i++;
 	}
 	while (src[j] != '\0')
 	{
-		dest[index] = src[j];
+		dest[i] = src[j];
 		j++;
-		index++;
+		i++;
 	}
 
-	dest[index] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
